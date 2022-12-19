a579d8275cf4f616a975517d593222c2c7095764
nicholas-guinther <nickguinther@protonmail.com>
2022-12-19 14:56:43 -0500

    Added clarifying comments

--- js/site.js
+++ js/site.js
@@ -1,43 +1,41 @@
 // Get the String from the page
 // Controller Function
-function getString(){
+function getString() {
+  document.getElementById("alert").classList.add("invisible");
 
-    document.getElementById("alert").classList.add("invisible");
+  let userString = document.getElementById("userString").value;
 
-    let userString = document.getElementById("userString").value;
+  let revString = reverseString(userString);
 
-    let revString = reverseString(userString);
-
-    displayString(revString);
+  displayString(revString);
 }
 
 // Reverse the String
 // Logic Function
-function reverseString(userString){
-    let revString = [];
+function reverseString(userString) {
+  let revString = [];
 
-    for (let index = userString.length - 1; index >= 0; index--) {
+  for (let index = userString.length - 1; index >= 0; index--) {
+    revString += userString[index];
+  }
 
-        revString += userString[index];
-    }
-
-    return revString;
+  return revString;
 }
 
 // Display the Reversed String
 // View Function
-function displayString(revString){
+function displayString(revString) {
+  // Write the message to the page
+  document.getElementById(
+    "msg"
+  ).innerHTML = `Your String Reversed is: ${revString}`;
 
-    // Write the message to the page
-    document.getElementById("msg").innerHTML = `Your String Reversed is: ${revString}`;
-
-    // Show the alert box
-    document.getElementById("alert").classList.remove("invisible");
-
+  // Show the alert box
+  document.getElementById("alert").classList.remove("invisible");
 }
 
-function clear(){
-
-    document.getElementById("userString").value = "";
-    document.getElementById("alert").classList.add("invisible");
+// Clears the input box and makes the alert window invisible
+function clear() {
+  document.getElementById("userString").value = "";
+  document.getElementById("alert").classList.add("invisible");
 }
